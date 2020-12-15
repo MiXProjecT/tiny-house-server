ef5266992b9433a62b903619748a40b8c238a17b
Vladimir <shumkov@amigoweb.ru>
2020-12-15 15:16:39 +0500

    Split resolvers on different folders

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -1,8 +1,9 @@
 import { ObjectId } from "mongodb";
 import { IResolvers } from "apollo-server-express"
-import { Database, Listing } from "../lib/types";
+import { Database, Listing } from "lib/types";
 
-export const resolvers: IResolvers = {
+
+export const listingResolvers: IResolvers = {
   Query: {
     listings: async (_root: undefined, _args: Record<string, never>, {db}: { db: Database }): Promise<Listing[]> => {
       return await db.listings.find({}).toArray();
