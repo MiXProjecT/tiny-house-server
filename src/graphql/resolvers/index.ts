8fc1eee0267e31af95a06343155d24e069e2bef6
Vladimir Shumkov <Vovan4ik112@gmail.com>
2021-01-16 18:21:06 +0500

    prepare graphql for google OAuth 2.0

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,4 +1,6 @@
 import merge from 'lodash.merge'
-import { listingResolvers } from "./Listing";
+import viewerResolvers from "./Viewer";
 
-export const resolvers = merge(listingResolvers)
+export const resolvers = merge(viewerResolvers);
+
+
