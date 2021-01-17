397cbd9c347759da0ddde8c3edd5376cd28222b2
Vladimir Shumkov <Vovan4ik112@gmail.com>
2021-01-18 04:17:16 +0500

    fix googleAuth bug and add return type for logIn function

--- src/lib/api/Google.ts
+++ src/lib/api/Google.ts
@@ -1,4 +1,9 @@
-import { google } from "googleapis";
+import {google, people_v1} from "googleapis";
+
+type PeopleData = {
+  user: people_v1.Schema$Person
+
+}
 
 const auth = new google.auth.OAuth2(
   process.env.G_CLIENT_ID,
@@ -16,12 +21,12 @@ const googleScope = auth.generateAuthUrl({
 
 const Google = {
   authUrl: googleScope,
-  logIn: async (code: string)  => {
+  logIn: async (code: string):Promise<PeopleData> => {
     const { tokens } = await auth.getToken(code);
 
     auth.setCredentials(tokens);
 
-    const { data } = await google.people({version: "v1"}).people.get({
+    const { data } = await google.people({version: "v1", auth: auth}).people.get({
       resourceName: 'people/me',
       personFields: 'emailAddresses,names,photos',
     })
