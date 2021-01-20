90855e4a1c3bd8f008dc066cc378cd652ac5df31
Vladimir Shumkov <Vovan4ik112@gmail.com>
2021-01-20 23:25:12 +0500

    build the User resolvers

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,6 +1,7 @@
 import merge from 'lodash.merge'
 import viewerResolvers from "./Viewer";
+import userResolvers from "./User";
 
-const resolvers = merge(viewerResolvers);
+const resolvers = merge(viewerResolvers, userResolvers);
 
 export default resolvers;
